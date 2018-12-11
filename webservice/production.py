445feea8601bd76f27e90eaf739449eec63922be
Olga Kunyavskaya <OChe24912@yandex.ru>
2018-12-11 05:51:21 -0500

    database info to production

---
+++ webservice/production.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app_db',
+        'USER': 'app_user', 
+        'PASSWORD': 'password', 
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
