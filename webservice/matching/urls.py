362a7da7b884333c4316799b71ed22aba7cdfb06
Olga Chernikova <Oche24912@yandex.ru>
2019-06-04 09:16:22 +0300

    update url

--- webservice/matching/urls.py
+++ webservice/matching/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main_page, name='main_page'),
-    url(r'^vis/(?P<pk>\d+)/$', views.vis_page, name='vis_page'),
-    url(r'^res/(?P<pk>\d+)/$', views.res_page, name='res_page')
+    url(r'^nerpa/$', views.main_page, name='main_page'),
+    url(r'^nerpa/vis/(?P<pk>\d+)/$', views.vis_page, name='vis_page'),
+    url(r'^nerpa/res/(?P<pk>\d+)/$', views.res_page, name='res_page')
 ]
--- webservice/nrpsmatche/urls.py
+++ webservice/nrpsmatche/urls.py
@@ -21,7 +21,7 @@ from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('nerpa/admin/', admin.site.urls),
     path(r'', include('matching.urls') )
 ]
 
