8312e0e0bdb55d39d0a12592f630068648ced403
Olga Kunyavskaya <Oche24912@yandex.ru>
2019-08-26 07:02:34 +0300

    update urls

--- webservice/matching/urls.py
+++ webservice/matching/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
     url(r'^nerpa/$', views.main_page, name='main_page'),
     url(r'^nerpa/vis/(?P<pk>\d+)/$', views.vis_page, name='vis_page'),
     url(r'^nerpa/res/(?P<pk>\d+)/$', views.res_page, name='res_page')
-]
+] + staticfiles_urlpatterns()
