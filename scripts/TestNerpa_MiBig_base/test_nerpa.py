2568f08fef6ef0b0c819481e839b6042191f5346
Olga Kunyavskaya <okunyavskaya@chihua.cab.spbu.ru>
2020-02-11 12:59:31 +0300

    clac rank

--- scripts/TestNerpa_MiBig_base/test_nerpa.py
+++ scripts/TestNerpa_MiBig_base/test_nerpa.py
@@ -35,6 +35,17 @@ def calcFDR(score_iswrong):
     plt.ylabel('FDR')
     plt.savefig('result/' + dirname + "/FDR_scores.png")
 
+#all_res[0] = genome id, all_res[1] = structure id, all_res[2] = score
+def getRank(all_res, BGC, score, for_structure=False):
+    pos = 0
+    if for_structure:
+        pos = 1
+
+    rank = 0
+    for res in all_res:
+        if (BGC in res[pos]) and float(res[2]) > float(score) + 0.0001:
+            rank += 1
+    return rank
 
 with open('mibig_base.csv', 'r') as f:
     csv_reader = csv.reader(f, delimiter=',')
@@ -44,6 +55,14 @@ with open('mibig_base.csv', 'r') as f:
         AA_info[row[0]] = [row[2], row[3], row[4], row[5]]
         list_AA.append(row[0])
         
+all_res = []
+with open("result/" + dirname + "/report.csv", 'r') as f:
+    csv_reader = csv.reader(f, delimiter=',')
+    for row in csv_reader:
+        if row[0] != 'score':
+            all_res.append([row[6], row[5], row[0]])
+
+
 result_match = {}
 cnt_all = 0
 cnt_found = 0
@@ -52,7 +71,7 @@ cnt_05per = 0
 score_iswrong = [] 
 with open("result/" + dirname + ".csv", 'w' ) as fw:
     csv_writer = csv.writer(fw, delimiter=',', quotechar='"')
-    csv_writer.writerow(['Accession', 'Main Product', 'Organism', 'Structure type', 'Molecule Length', 'Has Match', 'Score', 'Matched Length', 'Matched Percentage'])
+    csv_writer.writerow(['Accession', 'Main Product', 'Organism', 'Structure type', 'Molecule Length', 'Has Match', 'Score', 'Matched Length', 'Matched Percentage', 'Rank for Genome', 'Rank for Structure'])
     with open("result/" + dirname + "/report.csv", 'r') as f:
         csv_reader = csv.reader(f, delimiter=',')
         for row in csv_reader:
@@ -72,9 +91,9 @@ with open("result/" + dirname + ".csv", 'w' ) as fw:
             if (matched_percentage > 0.49):
                 cnt_05per += 1
 
-            csv_writer.writerow([mol_name, AA_info[mol_name][0], AA_info[mol_name][1], AA_info[mol_name][2], AA_info[mol_name][3], 'True', result_match[mol_name][0], result_match[mol_name][1], matched_percentage])
+            csv_writer.writerow([mol_name, AA_info[mol_name][0], AA_info[mol_name][1], AA_info[mol_name][2], AA_info[mol_name][3], 'True', result_match[mol_name][0], result_match[mol_name][1], matched_percentage, getRank(all_res, mol_name.split('.')[0], result_match[mol_name][0]), getRank(all_res, mol_name.split('.')[0], result_match[mol_name][0], True)])
         else:
-            csv_writer.writerow([mol_name] + AA_info[mol_name] + ['False', '0', '0', '0'])
+            csv_writer.writerow([mol_name] + AA_info[mol_name] + ['False', '0', '0', '0', '-1', '-1'])
 
 score_iswrong.sort(key=lambda x: (-x[0], x[1]))
 calcFDR(score_iswrong)
