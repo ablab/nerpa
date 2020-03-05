192fb4c0dcfd00432dc2a5449b1e3a1850a9e253
Olga Kunyavskaya <OChe24912@gmail.com>
2020-03-05 13:06:51 +0300

    calc running time

--- scripts/TestNerpa_MiBig_base/test_nerpa.sh
+++ scripts/TestNerpa_MiBig_base/test_nerpa.sh
@@ -1,6 +1,12 @@
 DATE=`date +"%d-%b-%Y_%H:%M"`
-
+START_TIME=$(date +%s)
 nerpa.py -p prediction.info -l structure.info --predictor NRPSPREDICTOR2 --insertion --deletion --modification -o ./result/res_$DATE
+END_TIME=$(date +%s)
+DIFF_TIME=$(($END_TIME - $START_TIME))
+
+echo "It tooks $DIFF_TIME seconds"
+echo "It tooks $DIFF_TIME seconds" > ./result/res_$DATE/running_time
+
 python3 ./test_nerpa.py res_$DATE
 python3 ./test_nerpa_details.py res_$DATE
 python3 ./merge_details_with_base.py res_$DATE
