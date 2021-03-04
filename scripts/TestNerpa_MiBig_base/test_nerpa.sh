3fa3a141a5450b8f04c4adff5a927680ae05346f
Olga Kunyavskaya <OChe24912@gmail.com>
2021-03-04 15:02:20 +0300

    run scripts in specific folder

--- scripts/TestNerpa_MiBig_base/test_nerpa.sh
+++ scripts/TestNerpa_MiBig_base/test_nerpa.sh
@@ -1,15 +1,14 @@
 DATE=`date +"%d-%b-%Y_%H:%M"`
 
-mkdir -p ./result/res_$DATE
+mkdir -p $TEST_MIBIG_RESULT_PATH/result/res_$DATE
 
 START_TIME=$(date +%s)
-#nerpa.py -p prediction.info -l structure.info.monomers --predictor NRPSPREDICTOR2 --monomer -o ./result/res_$DATE 2>./result/res_$DATE/debug.txt
-nerpa.py --antismash_output_list antismashOUTlist.info -l structure.info.monomers --skip_segment -10 --predictor NRPSPREDICTOR2 --monomer -o ./result/res_$DATE 2>./result/res_$DATE/debug.txt
+nerpa.py -a $TEST_MIBIG_DATA_PATH/prediction/ --smiles-tsv $TEST_MIBIG_DATA_PATH/mibig2019NRP.csv --col_smiles "SMILES" --col_id "Accession" --sep "," -o "$TEST_MIBIG_RESULT_PATH/result/res_$DATE"
 END_TIME=$(date +%s)
 DIFF_TIME=$(($END_TIME - $START_TIME))
 
 echo "It tooks $DIFF_TIME seconds"
-echo "It tooks $DIFF_TIME seconds" > ./result/res_$DATE/running_time
+echo "It tooks $DIFF_TIME seconds" > $TEST_MIBIG_RESULT_PATH/result/res_$DATE/running_time
 
 python3 ./test_nerpa.py res_$DATE
 python3 ./test_nerpa_details.py res_$DATE
