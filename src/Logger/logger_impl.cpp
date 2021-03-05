f128021b87b33934f8bb869249aa369781d07c98
Alexey Gurevich <alexeigurevich@gmail.com>
2021-03-05 21:59:31 +0300

    C++ Logger: removing boost dependency at the expense of a small functionality loss

--- src/Logger/logger_impl.cpp
+++ src/Logger/logger_impl.cpp
@@ -1,10 +1,5 @@
 //***************************************************************************
-//* Copyright (c) 2015 Saint Petersburg State University
-//* All Rights Reserved
-//* See file LICENSE for details.
-//***************************************************************************
 
-#include <boost/algorithm/string.hpp>
 #include "format.h"
 
 #include <string>
@@ -14,72 +9,10 @@
 
 #include "logger.hpp"
 
-
-#ifdef SPADES_USE_JEMALLOC
-# include <jemalloc/jemalloc.h>
-#endif
-
 namespace logging {
 
     properties::properties(level default_level)
             : def_level(default_level), all_default(true) {}
-
-    properties::properties(std::string filename, level default_level)
-            : def_level(default_level), all_default(true) {
-        if (filename.empty())
-            return;
-
-        std::ifstream in(filename.c_str());
-
-        std::map<std::string, level> remap = {
-                {"TRACE", L_TRACE},
-                {"DEBUG", L_DEBUG},
-                {"INFO" , L_INFO },
-                {"WARN" , L_WARN },
-                {"ERROR", L_ERROR}
-        };
-
-        while (!in.eof()) {
-            using namespace boost;
-
-            char buf [0x400] = {};
-            in.getline(buf, sizeof buf);
-
-            std::string str(buf);
-            trim(str);
-
-            if (str.empty() || boost::starts_with(str, "#"))
-                continue;
-
-            std::vector<std::string> entry;
-            split(entry, str, is_any_of("="));
-
-            if(entry.size() != 2)
-                throw std::runtime_error("invalid log file property entry: " + str);
-
-            trim    (entry[0]);
-            trim    (entry[1]);
-            to_upper(entry[1]);
-
-            auto it = remap.find(entry[1]);
-            if(it == remap.end())
-                throw std::runtime_error("invalid log file level description: " + entry[1]);
-
-            levels[entry[0]] = it->second;
-        }
-
-        auto def = levels.find("default");
-        if (def != levels.end())
-            def_level = def->second;
-
-        for (auto I = levels.begin(), E = levels.end(); I != E; ++I) {
-            if (I->second != def_level) {
-                all_default = false;
-                break;
-            }
-        }
-    }
-
 
     logger::logger(properties const& props)
             : props_(props) { }
@@ -96,20 +29,6 @@ namespace logging {
         return desired_level >= source_level;
     }
 
-#ifdef SPADES_USE_JEMALLOC
-
-    void logger::log(level desired_level, const char* file, size_t line_num, const char* source, const char* msg) {
-  double time = timer_.time();
-  const size_t *cmem = 0, *cmem_max = 0;
-  size_t clen = sizeof(cmem);
-
-  je_mallctl("stats.cactive", &cmem, &clen, NULL, 0);
-  je_mallctl("stats.cactive_max", &cmem_max, &clen, NULL, 0);
-
-  for (auto it = writers_.begin(); it != writers_.end(); ++it)
-    (*it)->write_msg(time, (*cmem) / 1024, (*cmem_max) / 1024, desired_level, file, line_num, source, msg);
-}
-#else
     void logger::log(level desired_level, const char* file, size_t line_num, const char* source, const char* msg) {
         double time = timer_.time();
         size_t max_rss = 0;//get_max_rss();
@@ -117,7 +36,6 @@ namespace logging {
         for (auto it = writers_.begin(); it != writers_.end(); ++it)
             (*it)->write_msg(time, max_rss, desired_level, file, line_num, source, msg);
     }
-#endif
 
 //
     void logger::add_writer(writer_ptr ptr)
@@ -131,8 +49,8 @@ namespace logging {
         return l;
     }
 
-    logger *create_logger(std::string filename, level default_level) {
-        return new logger(properties(filename, default_level));
+    logger *create_logger(level default_level) {
+        return new logger(properties(default_level));
     }
 
     void attach_logger(logger *lg) {
