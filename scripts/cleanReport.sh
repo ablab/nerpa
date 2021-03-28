1bc9b047310735eca5c35f23aa04981443bfc606
Olga Kunyavsksya <oche24912@yandex.ru>
2021-03-28 14:04:13 +0300

    delete unused scripts

--- scripts/cleanReport.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sed -i -e "s/graphs\///" $1
-sed -i -e "s/.gr//" $1 
-sed -i -e "s/\/media\/hosein\/My Passport\/hosein\/Desktop\/project\/sequence_data\/bacteria_complete\/antismash\///" $1
-sed -i -e "s/\/nrpspks_predictions_txt\/ctg1_nrpspredictor2_codes.txt//" $1
-
