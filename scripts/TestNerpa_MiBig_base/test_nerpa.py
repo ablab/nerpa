16a031afbc10a51a2d258b1e4def261af207441d
Olga Kunyavskaya <OChe24912@gmail.com>
2021-06-02 16:36:45 +0300

    fix running test nerpa script

--- scripts/TestNerpa_MiBig_base/test_nerpa.py
+++ scripts/TestNerpa_MiBig_base/test_nerpa.py
@@ -27,11 +27,11 @@ def getRank(all_res, BGC, score, for_structure=False):
     return rank
 
 with open(os.path.join(data_path, 'base.csv'), 'r') as f:
-    csv_reader = csv.reader(f, delimiter=',')
+    csv_reader = csv.reader(f, delimiter='\t')
     for row in csv_reader:
-        if (row[0] == 'Accession'):
+        if (row[0] == 'Accession' or row[0] == "Id"):
             continue
-        AA_info[row[0]] = [row[2], row[3], row[4], row[5]]
+        AA_info[row[0]] = [row[2], row[3], 1, 1]
         list_AA.append(row[0])
         
 all_res = []
