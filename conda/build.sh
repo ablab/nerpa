08a8d9b7b84b26dfbd084a762c1f2488aa74619a
Alexey Gurevich <alexeigurevich@gmail.com>
2021-10-04 16:18:01 +0300

    Conda: config files updated (this PR passes tests successfully)

--- conda/build.sh
+++ conda/build.sh
@@ -3,21 +3,19 @@
 set -e
 
 if [ "x$PREFIX" = "x" ]; then
-  PREFIX=`.`
+  PREFIX="$(pwd)"
 fi
+
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export LD_LIBRARY_PATH=${PREFIX}/lib:$LD_LIBRARY_PATH
 export CMAKE_LIBRARY_PATH=${PREFIX}/lib:$CMAKE_LIBRARY_PATH
 
 BUILD_DIR=build
-BASEDIR=`.`/`dirname $0`
+mkdir -p $BUILD_DIR
 
-rm -rf "$BASEDIR/$BUILD_DIR"
-mkdir -p "$BASEDIR/$BUILD_DIR"
-set -e
-cd "$BASEDIR/$BUILD_DIR"
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PREFIX" $* "$BASEDIR"
+cd $BUILD_DIR
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PREFIX" ../
 make -j 8
 make install
-cd $PREFIX
+cd ..
