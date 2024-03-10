70c615212c1ee3803e8bc65bed3611458ce55604
IlyaOlkhovsky <ilianolhin@gmail.com>
2024-03-10 18:45:23 +0100

    Integrated null scores, refined final output

--- src/write_results.py
+++ src/write_results.py
@@ -1,11 +1,26 @@
-from typing import List, Literal
+from typing import (
+    Callable,
+    Iterable,
+    List,
+    TypeVar,
+    Tuple
+)
 from src.NewMatcher.alignment_types import Match
+from src.data_types import BGC_Variant, NRP_Variant
+from src.nerpa_pipeline.handle_rban import GraphRecord
+
 from pathlib import Path
 from io import StringIO
 import csv
 import yaml
 from itertools import groupby
-from dataclasses import asdict
+
+T = TypeVar('T')
+U = TypeVar('U')
+
+def sort_groupby(items: Iterable[T],
+                 key: Callable[[T], U]) -> Iterable[Tuple[U, Iterable[T]]]:
+    return groupby(sorted(items, key=key, reverse=True), key=key)
 
 
 def write_yaml(data, out_file: Path):
@@ -16,6 +31,7 @@ def write_yaml(data, out_file: Path):
     # another hack (albeit less dirty) to forbid yaml creating references
     # https://stackoverflow.com/questions/13518819/avoid-references-in-pyyaml
     yaml.Dumper.ignore_aliases = lambda *args: True
+
     with open(out_file, 'w') as out:
         yaml.dump(data, out,
                   default_flow_style=None, sort_keys=False)
@@ -32,25 +48,35 @@ def build_report(matches: List[Match]) -> str:
     return result.getvalue()
 
 
-def write_matches_per_id(matches: List[Match], output_dir: Path,
-                         id_: Literal['BGC', 'NRP']):
-    get_id = (lambda m: m.bgc_variant.bgc_id) if id_ == 'BGC' else \
-        (lambda m: m.nrp_variant.nrp_id)
-    matches_per_id = groupby(sorted(matches, key=get_id, reverse=True),
-                              key=get_id)  # python sort is stable so groups will be sorted by score
-
-    for id_, id_matches in matches_per_id:
+def write_records_per_id(matches: List[T],
+                         output_dir: Path,
+                         get_id: Callable[[T], str]):
+    for id_, id_matches in sort_groupby(matches, get_id):  # python sort is stable so groups will be sorted by score
         (output_dir / Path(id_)).write_text('\n\n'.join(map(str, matches)))
 
 
-def write_results(matches: List[Match], output_dir: Path):
+def write_results(bgc_variants: List[BGC_Variant],
+                  nrp_variants: List[NRP_Variant],
+                  matches: List[Match],
+                  rban_graphs: List[GraphRecord],
+                  output_dir: Path):
     (output_dir / Path('report.tsv')).write_text(build_report(matches))
 
-    (output_dir / Path('details')).mkdir()
-    write_yaml(matches, output_dir / Path('matches.yaml'))
+    write_yaml(rban_graphs, output_dir / Path('rban_graphs.yaml'))
 
-    (output_dir / Path('details/per_BGC')).mkdir()
-    write_matches_per_id(matches, output_dir / Path('details/per_BGC'), id_='BGC')
+    (output_dir / Path('NRP_variants')).mkdir()
+    for nrp_id, nrp_id_variants in sort_groupby(nrp_variants, key=lambda nrp_variant: nrp_variant.nrp_id):
+        write_yaml(nrp_variants, output_dir / Path(f'NRP_variants/{nrp_id}.yaml'))
 
-    (output_dir / Path('details/per_NRP')).mkdir()
-    write_matches_per_id(matches, output_dir / Path('details/per_NRP'), id_='NRP')
+    (output_dir / Path('matches_details')).mkdir()
+    write_yaml(matches, output_dir / Path('matches_details/matches.yaml'))
+
+    (output_dir / Path('matches_details/per_BGC')).mkdir()
+    write_records_per_id(matches, output_dir / Path('matches_details/per_BGC'),
+                         get_id=lambda match: match.bgc_variant.bgc_id)
+
+    (output_dir / Path('matches_details/per_NRP')).mkdir()
+    write_records_per_id(matches, output_dir / Path('matches_details/per_NRP'),
+                         get_id=lambda match: match.nrp_variant.nrp_id)
+
+
